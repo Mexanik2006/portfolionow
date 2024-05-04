5a1e8cdcfb5b40db08c8aa797c75eb31fe2511e4
Mexanik2006 <mexhanik2006@gmail.com>
2024-05-04 11:19:09 +0500

    first commit

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 
-const Api = axios.create({
-    baseURL: 'http://localhost:5000'
-});
 // const Api = axios.create({
-//     baseURL: 'https://debt-project-backend.onrender.com'
+//     baseURL: 'http://localhost:5000'
 // });
+const Api = axios.create({
+    baseURL: 'https://portfolionow-back.onrender.com'
+});
 
 export default Api;
